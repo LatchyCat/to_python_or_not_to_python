a277ef26ec1f6f1f6b333d50da49597cbd5761be
oriel <orielsilvajr@gmail.com>
2023-12-23 17:09:20 -0500

    draft

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # to_python_or_not_to_python
+
+
+Daily python practice - Create a what do I need to learn to stop being ignorant of knowledge about python.
+
+
+I write down my ideas and what I started researching for the day
+I have millions of trillions of things to learn, so why not stop for a second and write 1 - 3 of those ideas down and use it to practice python?
+
+The idea of this repo series is to start practicing python/PaScale
+
+
---
+++ learning_python_and_PaScale12_23_2023.py
@@ -0,0 +1,33 @@
+
+
+# How to teach an AI to think faster in one(1) copy + Pasta🍝
+
+
+#   - *—@@—- Concept: —-@@—*
+# idea: - if everything is an object in javascript… (ReFraze sentence 1: would everything be an object in python?) - ReFraze sentence 2: is everything an object in python? Does python even have objects?
+
+# - *—@@—- Concept: —-@@—*
+# idea: -
+# boil_down_point: -
+
+
+
+# TODO: how_do_i_create_an_object{}_in_python 
+
+# TODO: how_do_i_create_a_function_in_python
+
+# TODO: how_do_i_create_a_’string’_in_python
+
+#  is it… Written in (pie_or ‘( ‘||’)’_python/PaScale)
+
+#  (*—@@—- Concept:  —-@@—*)
+
+# boil_down_point: - ()
+
+# idea: - Being a  x = ‘chatGPT.io’
+
+
+
+# TODO: Reach This!
+# Why does python not like spaces between the text?
+# How do I shut it off?
